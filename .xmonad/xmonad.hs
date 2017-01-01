741f3870ad3f58947fd549e40282bd81d251e7ad
wvffle <casper@wvffle.net>
2017-01-01 09:39:14 +0100

    bar!

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -9,6 +9,8 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.UrgencyHook
 import XMonad.Util.Run
 
+import System.IO
+
 baseConfig = desktopConfig
 
 startup = do
@@ -19,10 +21,22 @@ startup = do
   setDefaultCursor xC_left_ptr
 
 layout = spacing 10 $ gaps [(U, 34)] $ Tall 1 (3/100) (1/2)
-loghook h = dynamicLogWithPP $ defaultPP 
+loghook h = dynamicLogWithPP $ defaultPP {
+        ppCurrent           =   dzenColor "#ad0536" "#1b1918" . pad
+      , ppVisible           =   dzenColor "#444444" "#1b1918" . pad
+      , ppHidden            =   dzenColor "#666666" "#1b1918" . pad
+      , ppHiddenNoWindows   =   dzenColor "#444444" "#1b1918" . pad
+      , ppUrgent            =   dzenColor "#ff0000" "#1b1918" . pad
+      , ppWsSep             =   " "
+      , ppSep               =   "  ///  "
+
+      , ppTitle             =   (" " ++) . dzenColor "#444444" "#1b1918" . dzenEscape
+      , ppOutput            =   hPutStrLn h
+}
 
 main = do
-  status <- spawnPipe "dzen2 -x '10' -y '10' -h '24' -w '1260' -ta 'l' -fg '#ad0536' -bg '#1b1918'"
+  status <- spawnPipe "dzen2 -h '24' -w '980' -ta 'l' -fg '#ad0536' -bg '#1b1918'"
+  conky <- spawnPipe "conky -c ~/.xmonad/conky | dzen2 -x 980 -w 300 -h 24 -ta r -fg '#ad0536' -bg '#1b1918'"
 
   xmonad $ withUrgencyHook NoUrgencyHook $ baseConfig
     {
